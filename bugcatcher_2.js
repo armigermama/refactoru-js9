08ec0c7ae44932048afb92856ed72b24f0e5b066
Roni.Chen <roni.chen@comcast.net>
2014-01-20 16:06:08 -0700

    2nd bug fixed

--- bugcatcher_2.js
+++ bugcatcher_2.js
@@ -1,19 +1,27 @@
+/**
+A helper function for sending messages between two users. This should print out:
+
+0 is sending 'Hey there!' to 1
+0 is sending 'What's up?' to 2
+2 is sending 'Nothing is up.' to 0
+**/
+
 var sendMessage = function(myUserId, targetUserId, message, allUsers){
-  if(myUserId = null || targetUserId == null || message = null){
+  if (myUserId == null || targetUserId == null || message == null){
     return false;
   }
-  for(var i = 0; i < allUsers.length; i++){
-    if(allUsers[i].uid = targetUserId){
-      console.log(myUserId + " is sending '" + message + "' to " + allUsers[i].uid);
+  for (var i = 0; i < allUsers.length; i++){
+    if (allUsers[i].uid == targetUserId){
+      console.log(myUserId + " is sending '" + message + "'' to " + allUsers[i].uid);
     }
   }
 }
 
-var myUser1 = { uid: 0 };
-var myUser2 = { uid: 1 };
-var myUser3 = { uid: 2 };
+var myUser1 = {uid: 0};
+var myUser2 = {uid: 1};
+var myUser3 = {uid: 2};
 var allUsers = [myUser1, myUser2, myUser3];
 
-sendMessage(myUser1.uid, myUser2.uId, "Hey there!", allUsers);
-sendMessage(myUser1.uid, myUser3.uId, "What"s up?", allUsers);
-sendMessage(myUser3.uld, myUser1.uId, "Nothing is up.", allUsers);
+sendMessage(myUser1.uid, myUser2.uid, "Hey there!", allUsers);
+sendMessage(myUser1.uid, myUser3.uid, "What's up?", allUsers);
+sendMessage(myUser3.uid, myUser1.uid, "Nothing is up.", allUsers);
